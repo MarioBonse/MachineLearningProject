5453e33f705a9423cb30afbb8a2be8db7d5535b3
Mario Bonsembiante <mario.bonsembiante@gmail.com>
2018-12-01 13:03:47 +0100

    Fixed keras NN now it works, I don't know how but it works.

--- src/kerasNN.py
+++ src/kerasNN.py
@@ -8,15 +8,47 @@ import datacontrol
 import sklearn
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from sklearn.model_selection import StratifiedKFold
 
-NetworArchitecture = [100, 1000]
+
+NetworArchitecture = [100, 100, 100, 100]
 activation = "relu"
 lastlayeractivation = "softmax"
-eta = 0.1
-epochs = 100
-momentum = 0.1
-nesterov = False
-batch_size = 32
+eta = 0.01
+epochs = 500
+momentum = 0.5
+nesterov = True
+batch_size = 200
+
+def createModel(input_dimention, output_dimention):
+    model = Sequential()
+    model.add(Dense(units=NetworArchitecture[0], activation=activation, input_dim=input_dimention))
+    for node in NetworArchitecture[1:]:
+        model.add(Dense(units=node, activation=activation))
+    model.add(Dense(units=output_dimention))
+    model.compile(metrics=['acc'], loss=keras.losses.mean_absolute_error, optimizer=keras.optimizers.SGD(lr=eta, momentum=momentum, nesterov=nesterov))
+    return model
+
+def showresult(history):
+    loss_values = history.history['loss']
+    val_loss_value = history.history['val_loss']
+    epochs_array = range(1, epochs + 1)
+    plt.plot(epochs_array, loss_values, 'go', label='training loss')
+    plt.plot(epochs_array, val_loss_value, 'b', label='validation loss')
+    plt.title('yee')
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    acc_values = history.history['acc']
+    val_acc_value = history.history['val_acc']
+    plt.plot(epochs_array, acc_values, 'ro', label='training accuracy')
+    plt.plot(epochs_array, val_acc_value, 'r', label='validation accurcy')
+    plt.title('yee')
+    plt.xlabel('epochs')
+    plt.ylabel('acc')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
 def main():
     print("kerasNN\n")
@@ -26,32 +58,16 @@ def main():
         TrainingData = datacontrol.readFile("../data/ML-CUP18-TR.csv")
         TestData = datacontrol.readFile("../data/ML-CUP18-TS.csv")
         x_train, y_train = datacontrol.divide(TrainingData)
+        # preprocessing
+        x_train = sklearn.preprocessing.scale(x_train, axis=0, with_mean=True, with_std=True, copy=True)
         input_dimention = x_train.shape[1]
         output_dimention = y_train.shape[1]
-        # preprocessing
-        x_train = sklearn.preprocessing.scale(
-            x_train, axis=0, with_mean=True, with_std=True, copy=True)
-        #now define the model
-        model = Sequential()
-        model.add(Dense(units=NetworArchitecture[0], activation=activation, input_dim=input_dimention))
-        for node in NetworArchitecture[1:]:
-            model.add(Dense(units=node, activation=activation))
-        model.add(Dense(activation=activation, output_dim=output_dimention))
-        model.compile(metrics=['acc'], loss=keras.losses.mean_absolute_error,
-                    optimizer=keras.optimizers.SGD(lr=eta, momentum=momentum, nesterov=nesterov))
+        model = createModel(input_dimention, output_dimention)
         # x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
-        history = model.fit(x_train, y_train, validation_split=0.2,epochs=epochs, batch_size=batch_size, verbose=0)
+        history = model.fit(x_train, y_train, validation_split=0.2, epochs=epochs, batch_size=batch_size, verbose=1)
         loss_and_metrics = model.evaluate(x_train, y_train, batch_size=128)
-        loss_values = history.history['loss']
-        val_loss_value = history.history['val_loss']
-        epochs_array = range(1, epochs + 1)
-        plt.plot(epochs_array, loss_values, 'bo', label = 'training loss')
-        plt.plot(epochs_array, val_loss_value, 'b', label = 'validation loss')
-        plt.title('yee')
-        plt.xlabel('epochs')
-        plt.ylabel('loss')
-        plt.legend(['train', 'test'], loc='upper left')
-        plt.show()
+        showresult(history)
+        
 
 
 if __name__ == "__main__":
