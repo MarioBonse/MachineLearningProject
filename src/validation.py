695b7b6959c91e05eb06f285c9a4ed1ac0c06111
Mario Bonsembiante <mario.bonsembiante@gmail.com>
2018-12-18 12:28:29 +0100

    Mean euclidian error implemented

--- src/validation.py
+++ src/validation.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import KFold
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+from sklearn.model_selection import RepeatedKFold
 
 # function that train the algorithm with the function passed
 #
@@ -20,10 +21,45 @@ def kFoldCross(trainCallback, predictcallback, x_train, y_train, n_splits=4, pre
             x_test = scaler.transform(x_test)
         trainCallback(X_train, Y_train)
         y_predicted = predictcallback(x_test)
-        loss_val = (((y_predicted - y_test)**2).sum())/y_test[:,0].size
-        loss_training = (
-            ((predictcallback(X_train) - Y_train)**2).sum())/Y_train[:, 0].size
+        loss_val = MeanEuclidianError(y_predicted, y_test)
+        loss_training = MeanEuclidianError(predictcallback(X_train), Y_train)
         valLoss.append(loss_val)
         TRLoss.append( loss_training)
     return valLoss, TRLoss
+
+
+# RepeatedKFold
+# 
+
+def RepeatedKFoldCross(trainCallback, predictcallback, x_train, y_train, n_splits=5, preprocessing=True, n_repeats=4):
+    kf = RepeatedKFold(n_repeats=n_repeats, n_splits=n_splits)
+    if preprocessing:
+        scaler = StandardScaler()
+    valLoss = []
+    TRLoss = []
+
+    for train, test in kf.split(x_train):
+        X_train, x_test, Y_train, y_test = x_train[train], x_train[test], y_train[train], y_train[test]
+        if preprocessing:
+            scaler.fit(X_train)
+            X_train = scaler.transform(X_train)
+            x_test = scaler.transform(x_test)
+        trainCallback(X_train, Y_train)
+        y_predicted = predictcallback(x_test)
+        #mean euclidian error
+        loss_val = MeanEuclidianError(y_predicted, y_test)
+        loss_training = MeanEuclidianError(predictcallback(X_train), Y_train)
+        valLoss.append(loss_val)
+        TRLoss.append(loss_training)
+        print(loss_val)
+    return valLoss, TRLoss
+
         
+def MeanEuclidianError(X,Y):
+    #x, y in R2
+    #MEE = 1/N(sum(sqrt(x[i,0]-y[i,0]**2 + x[i,1]-y[i,1]**2))
+    #can be weitten in a faster way as 1/N(sumnp.sqrt(float(x.dot(x)) - 2 * float(x.dot(y)) + float(y.dot(y))))
+    out = 0
+    for x, y in zip(X, Y):
+        out += np.sqrt(float(x.dot(x)) - 2 * float(x.dot(y)) + float(y.dot(y)))
+    return out*2/(X.size)#X.size = 2*nelem -> i have to divide by two
